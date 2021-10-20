74627f537ef9ea0f229498d48c5846141d8d2e1e
elimarvin99 <elimarvin99@gmail.com>
2021-10-20 13:12:38 -0400

    fixed todo and game is operational

--- Platformer2D/Platformer2D.DesktopGL/Program.cs
+++ Platformer2D/Platformer2D.DesktopGL/Program.cs
@@ -8,9 +8,10 @@ namespace Platformer2D.DesktopGL
         static void Main()
         {
             // TODO: Create a new instance of the PlatformerGame Class called game (Tip: instantiating an object of type PlatformerGame)
-
+            using var game = new PlatformerGame() ;
 
             // TODO: Call the Run() method of the game object to run the game (Tip: accessing an object's members using dot notation)
+            game.Run();
         }
     }
 }
